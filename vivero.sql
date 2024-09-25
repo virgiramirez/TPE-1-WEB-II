bd433139a194b8a996fea36b381c9899798224ee
Ramirez Virginia Soledad <virginiasr19@gmail.com>
2024-09-25 19:42:23 -0300

    se añade nuevo archivo sql con ejemplos agregados

--- vivero.sql
+++ vivero.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-09-2024 a las 00:30:01
+-- Tiempo de generación: 26-09-2024 a las 00:40:51
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,94 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `vivero`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id_pedido` int(11) NOT NULL,
+  `fecha_pedido` date NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`id_pedido`, `fecha_pedido`, `estado`, `total`) VALUES
+(1, '2024-09-18', 'pedido', 16000),
+(2, '2024-08-25', 'finalizado', 20000);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `planta`
+--
+
+CREATE TABLE `planta` (
+  `id_planta` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `id_pedido` int(11) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `planta`
+--
+
+INSERT INTO `planta` (`id_planta`, `nombre`, `precio`, `id_pedido`, `stock`) VALUES
+(13, 'calathea', 16000, 1, 10),
+(14, 'albahaca ', 20000, 2, 50);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id_pedido`);
+
+--
+-- Indices de la tabla `planta`
+--
+ALTER TABLE `planta`
+  ADD PRIMARY KEY (`id_planta`),
+  ADD KEY `categoria` (`id_pedido`),
+  ADD KEY `categoria_2` (`id_pedido`),
+  ADD KEY `id_categoria` (`id_pedido`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `planta`
+--
+ALTER TABLE `planta`
+  MODIFY `id_planta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `planta`
+--
+ALTER TABLE `planta`
+  ADD CONSTRAINT `planta_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
